b87f5a781ab19ecaaec5714570141395beb61ef6
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-07 20:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere(event) {
     //Write your code here
 	event.preventDefault();
 	const radius =document.getElementById("radius").value;
-const volume =(4/3 Math.PI * radius * 3);
+const volume =(4/3 Math.PI * radius ** 3);
 document.getElementById("volume").value= volume.toFixed(4);
 	
 }
