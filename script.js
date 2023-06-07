c8f474eaf5d1684e4876760037eb27b0ecc42218
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-07 20:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
-} 
-
+	event.preventDefault();
+	const radius =document.getElementById("radius").value;
+const volume =(4/3 Math.PI * radius * 3);
+document.getElementById("volume").value= volume.toFixed(4);
+	
+}
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
